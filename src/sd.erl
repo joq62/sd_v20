8e828b86462e125db644d6fa543b15afd875e53d
joq62 <jakim.leche@gmail.com>
2023-01-20 22:06:21 +0100

    ud

--- src/sd.erl
+++ src/sd.erl
@@ -1,15 +1,12 @@
-%%% -------------------------------------------------------------------
-%%% Author  : uabjle
-%%% Description : resource discovery accroding to OPT in Action 
-%%% This service discovery is adapted to 
-%%% Type = application 
-%%% Instance ={ip_addr,{IP_addr,Port}}|{erlang_node,{ErlNode}}
-%%% 
-%%% Created : 10 dec 2012
-%%% -------------------------------------------------------------------
--module(sd). 
+%%% @author c50 <joq62@c50>
+%%% @copyright (C) 2023, c50
+%%% @doc
+%%%
+%%% @end
+%%% Created : 20 Jan 2023 by c50 <joq62@c50>
 
--behaviour(gen_server). 
+-module(sd).
+
 
 %% --------------------------------------------------------------------
 %% Include files
@@ -17,7 +14,7 @@
 
 
 %% --------------------------------------------------------------------
--define(SERVER,?MODULE).
+-define(SERVER,sd_server).
 
 
 %% External exports
@@ -32,51 +29,55 @@
 	 cast/4,
 	 all/0,
 
-	 appl_start/1,
+
 	 ping/0
 	]).
-
 
 -export([
 	 start/0,
 	 stop/0
 	]).
 
-
--export([init/1, handle_call/3,handle_cast/2, handle_info/2, terminate/2, code_change/3]).
-
--record(state, {
-	
-	       }).
-
-%% ====================================================================
-%% External functions
-%% ====================================================================
-appl_start([])->
-    application:start(?MODULE).
-
-%% ====================================================================
-%% Server functions
-%% ====================================================================
-%% Gen server functions
-
+%%--------------------------------------------------------------------
+%% @doc
+%% @spec
+%% @end
+%%--------------------------------------------------------------------
 start()-> gen_server:start_link({local, ?SERVER}, ?SERVER, [], []).
 stop()-> gen_server:call(?SERVER, {stop},infinity).
 
-%% ====================================================================
-%% Application handling
-%% ====================================================================
+%%--------------------------------------------------------------------
+%% @doc
+%% @spec
+%% @end
+%%--------------------------------------------------------------------
 
-%% ====================================================================
-%% Support functions
-%% ====================================================================
-%%---------------------------------------------------------------
-%% Function:all_specs()
-%% @doc: all service specs infromation       
-%% @param: non 
-%% @returns:State#state.service_specs_info
-%%
-%%---------------------------------------------------------------
+
+all()->
+    sd_server:all().
+get_node(WantedApp)->
+    sd_server:get_node(WantedApp).
+get_node_on_node(WantedApp,WantedNode)->
+    sd_server:get_node_on_node(WantedApp,WantedNode).
+get_node_on_host(WantedApp,WantedHost)->
+    sd_server:get_node_on_host(WantedApp,WantedHost).
+get_node_host(WantedApp)->
+    sd_server:get_node_host(WantedApp).
+get_node_host_on_node(WantedApp,WantedNode)->
+    sd_server:get_node_host_on_node(WantedApp,WantedNode).
+get_node_host_on_host(WantedApp,WantedHost)->
+    sd_server:get_node_host_on_host(WantedApp,WantedHost).
+
+
+call(App,M,F,A,T)->
+    sd_server:call(App,M,F,A,T).
+
+
+
+cast(App,M,F,A)->
+    sd_server:cast(App,M,F,A).
+
+
 
 %% 
 %% @doc:check if service is running
@@ -87,202 +88,3 @@ stop()-> gen_server:call(?SERVER, {stop},infinity).
 ping()-> 
     gen_server:call(?SERVER, {ping},infinity).
 
-%% ====================================================================
-%% Gen Server functions
-%% ====================================================================
-%% --------------------------------------------------------------------
-%% Function: init/1
-%% Description: Initiates the server
-%% Returns: {ok, State}          |
-%%          {ok, State, Timeout} |
-%%          ignore               |
-%%          {stop, Reason}
-%% --------------------------------------------------------------------
-init([]) ->
-    case code:is_loaded(nodelog) of
-	false->
-	    be_silent;
-	_->
-	    rpc:cast(node(),nodelog,log,[notice,?MODULE_STRING,?LINE,
-						{"OK, started server at node  ",?MODULE," ",node()}])
-    end,
-    {ok, #state{}
-    }.
-
-%% --------------------------------------------------------------------
-%% Function: handle_call/3
-%% Description: Handling call messages
-%% Returns: {reply, Reply, State}          |
-%%          {reply, Reply, State, Timeout} |
-%%          {noreply, State}               |
-%%          {noreply, State, Timeout}      |
-%%          {stop, Reason, Reply, State}   | (terminate/2 is called)
-%%          {stop, Reason, State}            (terminate/2 is called)
-%% --------------------------------------------------------------------
-handle_call({ping},_From, State) ->
-    Reply=pong,
-    {reply, Reply, State};
-
-handle_call({stopped},_From, State) ->
-    Reply=ok,
-    {reply, Reply, State};
-
-
-handle_call({not_implemented},_From, State) ->
-    Reply=not_implemented,
-    {reply, Reply, State};
-
-handle_call({stop}, _From, State) ->
-    {stop, normal, shutdown_ok, State};
-
-handle_call(Request, From, State) ->
-    %rpc:cast(node(),log,log,[?Log_ticket("unmatched call",[Request, From])]),
-    Reply = {ticket,"unmatched call",Request, From},
-    {reply, Reply, State}.
-
-%% --------------------------------------------------------------------
-%% Function: handle_cast/2
-%% Description: Handling cast messages
-%% Returns: {noreply, State}          |
-%%          {noreply, State, Timeout} |
-%%          {stop, Reason, State}            (terminate/2 is called)
-%% --------------------------------------------------------------------
-handle_cast(_Msg, State) ->
-  %  rpc:cast(node(),log,log,[?Log_ticket("unmatched cast",[Msg])]),
-    {noreply, State}.
-
-%% --------------------------------------------------------------------
-%% Function: handle_info/2
-%% Description: Handling all non call/cast messages
-%% Returns: {noreply, State}          |
-%%          {noreply, State, Timeout} |
-%%          {stop, Reason, State}            (terminate/2 is called)
-%% --------------------------------------------------------------------
-handle_info(_Info, State) ->
-    %rpc:cast(node(),log,log,[?Log_ticket("unmatched info",[Info])]),
-    {noreply, State}.
-
-%% --------------------------------------------------------------------
-%% Function: terminate/2
-%% Description: Shutdown the server
-%% Returns: any (ignored by gen_server)
-%% --------------------------------------------------------------------
-terminate(_Reason, _State) ->
-    ok.
-
-%% --------------------------------------------------------------------
-%% Func: code_change/3
-%% Purpose: Convert process state when code is changed
-%% Returns: {ok, NewState}
-%% --------------------------------------------------------------------
-code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
-
-
-%% --------------------------------------------------------------------
-%% Function:start/0 
-%% Description: Initiate the eunit tests, set upp needed processes etc
-%% Returns: non
-%% --------------------------------------------------------------------
-call(App,M,F,A,T)->
-    Result=case rpc:call(node(),sd,get_node_host,[App],T) of
-	       {badrpc,Reason}->
-		   {error,[{badrpc,Reason}]};
-	       []->
-		   [];
-	       [{Node,_}|_]->
-		   rpc:call(Node,M,F,A,T)
-	   end,
-    Result.
-
-	%% --------------------------------------------------------------------
-%% Function:start/0 
-%% Description: Initiate the eunit tests, set upp needed processes etc
-%% Returns: non
-%% --------------------------------------------------------------------
-cast(App,M,F,A)->
-    Result=case rpc:call(node(),sd,get_node_host,[App],5*1000) of
-	       {badrpc,Reason}->
-		   {badrpc,Reason};
-	       []->
-		   {error,[eexists,App,?FUNCTION_NAME,?MODULE,?LINE]};
-	       [{Node,_}|_]->
-		   rpc:cast(Node,M,F,A)
-	   end,
-    Result.			   
-
-%% --------------------------------------------------------------------
-%% Function:start/0 
-%% Description: Initiate the eunit tests, set upp needed processes etc
-%% Returns: non
-%% --------------------------------------------------------------------
-all()->
-    Apps=[{Node,rpc:call(Node,net,gethostname,[],5*1000),rpc:call(Node,application,which_applications,[],5*1000)}||Node<-[node()|nodes()]],
-    AvailableNodes=[{Node,HostName,AppList}||{Node,{ok,HostName},AppList}<-Apps,
-				    AppList/={badrpc,nodedown}],
-    AvailableNodes.
-    
-
-
-get_node(WantedApp)->
-    Apps=[{Node,rpc:call(Node,application,which_applications,[],5*1000)}||Node<-[node()|nodes()]],
-    AvailableNodes=[Node||{Node,AppList}<-Apps,
-				     AppList/={badrpc,nodedown},
-				     AppList/={badrpc,timeout},
-				     true==lists:keymember(WantedApp,1,AppList)],
-    AvailableNodes.
-
-get_node_on_node(WantedApp,WantedNode)->
-
-    Apps=[{Node,rpc:call(Node,application,which_applications,[],5*1000)}||Node<-[node()|nodes()]],
-    AvailableNodes=[Node||{Node,AppList}<-Apps,
-			  AppList/={badrpc,nodedown},
-			  AppList/={badrpc,timeout},
-			  true==lists:keymember(WantedApp,1,AppList),
-			  Node==WantedNode],
-    AvailableNodes.
-
-get_node_on_host(WantedApp,WantedHost)->
-    Apps=[{Node,rpc:call(Node,net,gethostname,[],5*1000),
-	   rpc:call(Node,application,which_applications,[],5*1000)}||Node<-[node()|nodes()]],
-    AvailableNodes=[Node||{Node,{ok,HostName},AppList}<-Apps,
-				     AppList/={badrpc,nodedown},
-				     AppList/={badrpc,timeout},
-				     true=:=lists:keymember(WantedApp,1,AppList),
-				     HostName=:=WantedHost],
-    AvailableNodes.
-	  
-
-get_node_host(WantedApp)->
-    Apps=[{Node,rpc:call(Node,net,gethostname,[],5*1000),
-	   rpc:call(Node,application,which_applications,[],5*1000)}||Node<-[node()|nodes()]],
-    AvailableNodes=[{Node,HostName}||{Node,{ok,HostName},AppList}<-Apps,
-				     AppList/={badrpc,nodedown},
-				     AppList/={badrpc,timeout},
-				     true==lists:keymember(WantedApp,1,AppList)],
-    AvailableNodes.
-
-get_node_host_on_node(WantedApp,WantedNode)->
-
-    Apps=[{Node,rpc:call(Node,net,gethostname,[],5*1000),
-	   rpc:call(Node,application,which_applications,[],5*1000)}||Node<-[node()|nodes()]],
-    AvailableNodes=[{Node,HostName}||{Node,{ok,HostName},AppList}<-Apps,
-				     AppList/={badrpc,nodedown},
-				     AppList/={badrpc,timeout},
-				     true==lists:keymember(WantedApp,1,AppList),
-				     Node==WantedNode],
-    AvailableNodes.
-
-get_node_host_on_host(WantedApp,WantedHost)->
-    Apps=[{Node,rpc:call(Node,net,gethostname,[],5*1000),
-	   rpc:call(Node,application,which_applications,[],5*1000)}||Node<-[node()|nodes()]],
-    AvailableNodes=[{Node,HostName}||{Node,{ok,HostName},AppList}<-Apps,
-				     AppList/={badrpc,nodedown},
-				     AppList/={badrpc,timeout},
-				     true=:=lists:keymember(WantedApp,1,AppList),
-				     HostName=:=WantedHost],
-    AvailableNodes.
-	  
-%% --------------------------------------------------------------------
-%%% Internal functions
-%% --------------------------------------------------------------------
